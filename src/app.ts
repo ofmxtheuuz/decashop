16bb566699ebee5f2527b37afdac55ad89aa03bd
Matheus Piccoli <mxtheuzchika@gmail.com>
2023-03-05 16:13:31 -0300

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,39 @@
+import express, { Request, Response, NextFunction } from 'express'
+const app = express()
+
+require("dotenv").config();
+
+import { server, database, error } from './helpers/messages';
+import * as handlebars from 'express-handlebars'
+import * as path from 'path';
+import * as session from 'express-session';
+import * as parser from "cookie-parser"
+import * as logger from "morgan"
+import config from "config";
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.engine('handlebars', handlebars.engine({ defaultLayout: "main" }));
+app.set('view engine', 'handlebars');
+
+
+app.use(session.default({
+    secret: process.env.SESSION_SECRET || "347GJ86259SF3DS42657GABN235GAS672A3ASD5FD78567",
+    resave: true,
+    saveUninitialized: true,
+    cookie: { maxAge: 2 * 60 * 1000 }
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger.default("dev"))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(parser.default());
+
+import client from "./routes/client";
+app.get('/', client)
+
+const PORT = process.env.PORT || 3333 
+
+app.listen(PORT, () => {
+    server(`Servidor aberto e operando na porta ${PORT}: http://localhost:${PORT}`);
+})
